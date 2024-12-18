88cc279c918e5e23b7e5b27f60cab46e8e710620
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:33:52 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/binders/BindIterableAsPayloadToDeleteRequest.java

--- apis/s3/src/main/java/org/jclouds/s3/binders/BindIterableAsPayloadToDeleteRequest.java
+++ apis/s3/src/main/java/org/jclouds/s3/binders/BindIterableAsPayloadToDeleteRequest.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.binders;
 
 import static com.google.common.base.Charsets.UTF_8;
@@ -44,14 +29,24 @@ public class BindIterableAsPayloadToDeleteRequest implements Binder {
    @SuppressWarnings("unchecked")
    @Override
    public <R extends HttpRequest> R bindToRequest(R request, Object input) {
+      validateInput(request, input);
+
+      Iterable<String> keys = (Iterable<String>) input;
+      String content = createXMLContent(keys);
+
+      Payload payload = createPayload(content);
+      request.setPayload(payload);
+      return request;
+   }
+
+   private void validateInput(Object request, Object input) {
       checkArgument(checkNotNull(input, "input is null") instanceof Iterable,
          "this binder is only valid for an Iterable");
       checkNotNull(request, "request is null");
+      checkArgument(!Iterables.isEmpty((Iterable<?>) input), "The list of keys should not be empty.");
+   }
 
-      Iterable<String> keys = (Iterable<String>) input;
-      checkArgument(!Iterables.isEmpty(keys), "The list of keys should not be empty.");
-
-      String content;
+   private String createXMLContent(Iterable<String> keys) {
       try {
          Document document = createDocument();
          Element rootNode = elem(document, "Delete", document);
@@ -59,17 +54,17 @@ public class BindIterableAsPayloadToDeleteRequest implements Binder {
             Element objectNode = elem(rootNode, "Object", document);
             elemWithText(objectNode, "Key", key, document);
          }
-
-         content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + asString(document);
-      } catch (ParserConfigurationException | TransformerException pce) {
-         throw Throwables.propagate(pce);
+         return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + asString(document);
+      } catch (ParserConfigurationException | TransformerException e) {
+         throw Throwables.propagate(e);
       }
+   }
 
+   private Payload createPayload(String content) {
       Payload payload = Payloads.newStringPayload(content);
       payload.getContentMetadata().setContentType(MediaType.TEXT_XML);
       byte[] md5 = md5().hashString(content, UTF_8).asBytes();
       payload.getContentMetadata().setContentMD5(md5);
-      request.setPayload(payload);
-      return request;
+      return payload;
    }
 }
