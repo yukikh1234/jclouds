cbc546cbcf41c13e3ea6bce86ed613353ed6b175
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:14:56 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListAlarmHistoryResponseHandler.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListAlarmHistoryResponseHandler.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListAlarmHistoryResponseHandler.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.xml;
 
 import java.util.Set;
@@ -61,18 +46,21 @@ public class ListAlarmHistoryResponseHandler
    @Override
    public void endElement(String uri, String name, String qName) throws SAXException {
       if (inAlarmHistoryItems) {
-         if (qName.equals("AlarmHistoryItems")) {
-            inAlarmHistoryItems = false;
-         } else if (qName.equals("member")) {
-            alarmHistoryItems.add(alarmHistoryItemHandler.getResult());
-         } else {
-            alarmHistoryItemHandler.endElement(uri, name, qName);
-         }
+         handleAlarmHistoryItemsEnd(qName);
       } else if (qName.equals("NextToken")) {
          nextToken = SaxUtils.currentOrNull(currentText);
       }
-
       currentText.setLength(0);
+   }
+
+   private void handleAlarmHistoryItemsEnd(String qName) throws SAXException {
+      if (qName.equals("AlarmHistoryItems")) {
+         inAlarmHistoryItems = false;
+      } else if (qName.equals("member")) {
+         alarmHistoryItems.add(alarmHistoryItemHandler.getResult());
+      } else {
+         alarmHistoryItemHandler.endElement(null, null, qName);
+      }
    }
 
    @Override
@@ -88,10 +76,9 @@ public class ListAlarmHistoryResponseHandler
    public IterableWithMarker<AlarmHistoryItem> getResult() {
       IterableWithMarker<AlarmHistoryItem> result = IterableWithMarkers.from(alarmHistoryItems, nextToken);
 
-      alarmHistoryItems = Sets.newLinkedHashSet();
+      alarmHistoryItems.clear();
       nextToken = null;
 
       return result;
    }
-
 }
