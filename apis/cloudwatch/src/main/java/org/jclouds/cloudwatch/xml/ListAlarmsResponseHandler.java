c8a90b237a4015c7d824dce217d694eff4e45540
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:20:52 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListAlarmsResponseHandler.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListAlarmsResponseHandler.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListAlarmsResponseHandler.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.xml;
 
 import java.util.Set;
@@ -61,18 +46,21 @@ public class ListAlarmsResponseHandler
    @Override
    public void endElement(String uri, String name, String qName) throws SAXException {
       if (inMetricAlarms) {
-         if (qName.equals("MetricAlarms")) {
-            inMetricAlarms = false;
-         } else if (qName.equals("member") && !metricAlarmHandler.shouldHandleMemberTag()) {
-            alarms.add(metricAlarmHandler.getResult());
-         } else {
-            metricAlarmHandler.endElement(uri, name, qName);
-         }
+         handleMetricAlarmsEndElement(uri, name, qName);
       } else if (qName.equals("NextToken")) {
          nextToken = SaxUtils.currentOrNull(currentText);
       }
-
       currentText.setLength(0);
+   }
+
+   private void handleMetricAlarmsEndElement(String uri, String name, String qName) throws SAXException {
+      if (qName.equals("MetricAlarms")) {
+         inMetricAlarms = false;
+      } else if (qName.equals("member") && !metricAlarmHandler.shouldHandleMemberTag()) {
+         alarms.add(metricAlarmHandler.getResult());
+      } else {
+         metricAlarmHandler.endElement(uri, name, qName);
+      }
    }
 
    @Override
@@ -93,5 +81,4 @@ public class ListAlarmsResponseHandler
 
       return result;
    }
-
 }
