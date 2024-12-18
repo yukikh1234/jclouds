7b8e293b0c0364a6b6968b05746f5e093c7eeeef
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 16:54:10 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/internal/BackoffOnNotFoundWhenGetBucketACL.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/internal/BackoffOnNotFoundWhenGetBucketACL.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/internal/BackoffOnNotFoundWhenGetBucketACL.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.blobstore.internal;
 
 import static com.google.common.base.Throwables.propagate;
@@ -27,7 +12,6 @@ import org.jclouds.s3.domain.AccessControlList;
 import com.google.common.annotations.Beta;
 import com.google.common.cache.CacheLoader;
 
-
 @Beta
 public class BackoffOnNotFoundWhenGetBucketACL extends CacheLoader<String, AccessControlList> {
    private final S3Client client;
@@ -40,21 +24,32 @@ public class BackoffOnNotFoundWhenGetBucketACL extends CacheLoader<String, Acces
 
    @Override
    public AccessControlList load(String bucketName) {
-      ResourceNotFoundException last = null;
+      return attemptToGetBucketACL(bucketName);
+   }
+
+   private AccessControlList attemptToGetBucketACL(String bucketName) {
+      ResourceNotFoundException lastException = null;
       for (int currentTries = 0; currentTries < maxTries; currentTries++) {
          try {
             return client.getBucketACL(bucketName);
          } catch (ResourceNotFoundException e) {
-            imposeBackoffExponentialDelay(100L, 200L, 2, currentTries, maxTries);
-            last = e;
+            performBackoff(currentTries);
+            lastException = e;
          }
       }
-      throw last;
+      throw lastException;
    }
 
-   private static void imposeBackoffExponentialDelay(long period, long maxPeriod, int pow, int failureCount, int max) {
+   private void performBackoff(int currentTries) {
+      long period = 100L;
+      long maxPeriod = 200L;
+      int pow = 2;
+      imposeBackoffExponentialDelay(period, maxPeriod, pow, currentTries);
+   }
+
+   private static void imposeBackoffExponentialDelay(long period, long maxPeriod, int pow, int failureCount) {
       long delayMs = (long) (period * Math.pow(failureCount, pow));
-      delayMs = delayMs > maxPeriod ? maxPeriod : delayMs;
+      delayMs = Math.min(delayMs, maxPeriod);
       try {
          Thread.sleep(delayMs);
       } catch (InterruptedException e) {
