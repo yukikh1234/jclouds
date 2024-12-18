55f179ec4c96ba8ac02b4e0f771681c83459443f
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:52:02 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/functions/AssignCorrectHostnameForBucket.java

--- apis/s3/src/main/java/org/jclouds/s3/functions/AssignCorrectHostnameForBucket.java
+++ apis/s3/src/main/java/org/jclouds/s3/functions/AssignCorrectHostnameForBucket.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.functions;
 
 import java.net.URI;
@@ -40,11 +25,16 @@ public final class AssignCorrectHostnameForBucket implements Function<Object, UR
 
    @Override
    public URI apply(Object from) {
-      String bucket = from.toString();
-      Optional<String> region = bucketToRegion.apply(bucket);
-      if (region.isPresent()) {
-         return delegate.apply(region.get());
-      }
-      return delegate.apply(null);
+      String bucketName = from.toString();
+      Optional<String> region = getRegionForBucket(bucketName);
+      return applyDelegate(region);
+   }
+
+   private Optional<String> getRegionForBucket(String bucketName) {
+      return bucketToRegion.apply(bucketName);
+   }
+
+   private URI applyDelegate(Optional<String> region) {
+      return delegate.apply(region.orNull());
    }
 }
