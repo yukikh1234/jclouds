ce7f044b910dbf5c873b67b6e05be8260d3e9c52
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:21:49 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/GetMetricStatisticsResponseHandler.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/GetMetricStatisticsResponseHandler.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/GetMetricStatisticsResponseHandler.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.xml;
 
 import java.util.Set;
@@ -48,14 +33,25 @@ public class GetMetricStatisticsResponseHandler extends ParseSax.HandlerWithResu
 
    @Override
    public void endElement(String uri, String localName, String qName) throws SAXException {
+      handleEndElement(uri, localName, qName);
+   }
+
+   private void handleEndElement(String uri, String localName, String qName) throws SAXException {
       datapointHandler.endElement(uri, localName, qName);
-      if (qName.equals("member")) {
-         this.datapoints.add(datapointHandler.getResult());
+      if (isMemberElement(qName)) {
+         addCurrentDatapoint();
       }
+   }
+
+   private boolean isMemberElement(String qName) {
+      return "member".equals(qName);
+   }
+
+   private void addCurrentDatapoint() {
+      this.datapoints.add(datapointHandler.getResult());
    }
 
    public void characters(char[] ch, int start, int length) {
       datapointHandler.characters(ch, start, length);
    }
-
 }
