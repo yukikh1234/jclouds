23d74408d2af67728783e3b6028ab3118435573f
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 13:51:37 -0500

    Your changing file apis/byon/src/main/java/org/jclouds/byon/functions/NodeToNodeMetadata.java

--- apis/byon/src/main/java/org/jclouds/byon/functions/NodeToNodeMetadata.java
+++ apis/byon/src/main/java/org/jclouds/byon/functions/NodeToNodeMetadata.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.byon.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -24,6 +9,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.Optional;
 
 import jakarta.annotation.Resource;
 import jakarta.inject.Inject;
@@ -74,60 +60,72 @@ public class NodeToNodeMetadata implements Function<Node, NodeMetadata> {
    @Override
    public NodeMetadata apply(Node from) {
       NodeMetadataBuilder builder = new NodeMetadataBuilder();
+      populateBasicDetails(builder, from);
+      populateOperatingSystem(builder, from);
+      populateCredentials(builder, from);
+      return builder.build();
+   }
+
+   private void populateBasicDetails(NodeMetadataBuilder builder, Node from) {
       builder.ids(from.getId());
       builder.name(from.getName());
       builder.loginPort(from.getLoginPort());
       builder.hostname(from.getHostname());
-      builder.location(findLocationWithId(from.getLocationId()));
+      builder.location(findLocationWithId(from.getLocationId()).orElse(location.get()));
       builder.group(from.getGroup());
       builder.tags(from.getTags());
       builder.userMetadata(from.getMetadata());
-      builder.operatingSystem(OperatingSystem.builder().arch(from.getOsArch()).family(
-               OsFamily.fromValue(from.getOsFamily())).description(from.getOsDescription())
-               .version(from.getOsVersion()).build());
       builder.status(Status.RUNNING);
-      builder.publicAddresses(ImmutableSet.<String> of(from.getHostname()));
-
-      if (from.getUsername() != null) {
-         Builder credBuilder = LoginCredentials.builder().user(from.getUsername());
-         if (from.getCredentialUrl() != null) {
-            try {
-               credBuilder.credential(Strings2.toStringAndClose(slurp.apply(from.getCredentialUrl())));
-            } catch (IOException e) {
-               logger.error(e, "URI could not be read: %s", from.getCredentialUrl());
-            }
-         } else if (from.getCredential() != null) {
-            credBuilder.credential(from.getCredential());
-         }
-         if (from.getSudoPassword() != null) {
-            credBuilder.password(from.getSudoPassword());
-            credBuilder.authenticateSudo(true);
-         }
-         LoginCredentials creds = credBuilder.build();
-         builder.credentials(creds);
-         credentialStore.put("node#" + from.getId(), creds);
-      }
-
-      return builder.build();
+      builder.publicAddresses(ImmutableSet.of(from.getHostname()));
    }
 
-   private Location findLocationWithId(final String locationId) {
-      if (locationId == null)
-         return location.get();
-      try {
-         Location location = Iterables.find(locations.get(), new Predicate<Location>() {
+   private void populateOperatingSystem(NodeMetadataBuilder builder, Node from) {
+      builder.operatingSystem(OperatingSystem.builder()
+               .arch(from.getOsArch())
+               .family(OsFamily.fromValue(from.getOsFamily()))
+               .description(from.getOsDescription())
+               .version(from.getOsVersion())
+               .build());
+   }
 
+   private void populateCredentials(NodeMetadataBuilder builder, Node from) {
+      if (from.getUsername() == null) return;
+
+      Builder credBuilder = LoginCredentials.builder().user(from.getUsername());
+      loadCredential(from, credBuilder);
+      if (from.getSudoPassword() != null) {
+         credBuilder.password(from.getSudoPassword());
+         credBuilder.authenticateSudo(true);
+      }
+      LoginCredentials creds = credBuilder.build();
+      builder.credentials(creds);
+      credentialStore.put("node#" + from.getId(), creds);
+   }
+
+   private void loadCredential(Node from, Builder credBuilder) {
+      if (from.getCredentialUrl() != null) {
+         try {
+            credBuilder.credential(Strings2.toStringAndClose(slurp.apply(from.getCredentialUrl())));
+         } catch (IOException e) {
+            logger.error(e, "URI could not be read: %s", from.getCredentialUrl());
+         }
+      } else if (from.getCredential() != null) {
+         credBuilder.credential(from.getCredential());
+      }
+   }
+
+   private Optional<Location> findLocationWithId(final String locationId) {
+      if (locationId == null) return Optional.empty();
+      try {
+         return Optional.of(Iterables.find(locations.get(), new Predicate<Location>() {
             @Override
             public boolean apply(Location input) {
                return input.getId().equals(locationId);
             }
-
-         });
-         return location;
-
+         }));
       } catch (NoSuchElementException e) {
          logger.debug("couldn't match instance location %s in: %s", locationId, locations.get());
-         return location.get();
+         return Optional.empty();
       }
    }
 }
