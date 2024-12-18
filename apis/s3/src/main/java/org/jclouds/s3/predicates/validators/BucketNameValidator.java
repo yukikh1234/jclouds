8d58c9546f5260f10c63690617663acae17c1ef0
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 15:40:56 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/predicates/validators/BucketNameValidator.java

--- apis/s3/src/main/java/org/jclouds/s3/predicates/validators/BucketNameValidator.java
+++ apis/s3/src/main/java/org/jclouds/s3/predicates/validators/BucketNameValidator.java
@@ -1,25 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.predicates.validators;
 
 import jakarta.inject.Inject;
-
 import org.jclouds.predicates.Validator;
-
 import com.google.common.base.CharMatcher;
 import com.google.inject.Singleton;
 
@@ -44,20 +27,39 @@ public class BucketNameValidator extends Validator<String> {
 
    @Override
    public void validate(String name) {
-      if (name == null) {
+      if (isNull(name)) {
          throw exception("", "Can't be null");
-      } else if (name.length() < 3) {
+      }
+      if (isTooShort(name)) {
          throw exception(name, "Can't be less than 3 characters");
-      } else if (name.length() > 255) {
+      }
+      if (isTooLong(name)) {
          throw exception(name, "Can't be over 255 characters");
-      } else if (!MATCHER.matchesAllOf(name)) {
+      }
+      if (hasIllegalCharacters(name)) {
          throw exception(name, "Illegal character");
       }
    }
 
+   private boolean isNull(String name) {
+      return name == null;
+   }
+
+   private boolean isTooShort(String name) {
+      return name.length() < 3;
+   }
+
+   private boolean isTooLong(String name) {
+      return name.length() > 255;
+   }
+
+   private boolean hasIllegalCharacters(String name) {
+      return !MATCHER.matchesAllOf(name);
+   }
+
    private static IllegalArgumentException exception(String containerName, String reason) {
       return new IllegalArgumentException(String.format(
-            "Object '%s' doesn't match S3 bucket bucket naming convention. " +
+            "Object '%s' doesn't match S3 bucket naming convention. " +
             "Reason: %s. For more info, please refer to https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html",
             containerName, reason));
    }
