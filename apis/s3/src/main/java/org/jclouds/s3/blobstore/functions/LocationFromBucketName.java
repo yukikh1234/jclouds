e563d38341cd7d14a2f9bcf45d40aed95387be0d
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:03:55 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/LocationFromBucketName.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/LocationFromBucketName.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/LocationFromBucketName.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.blobstore.functions;
 
 import static com.google.common.collect.Iterables.find;
@@ -53,18 +38,29 @@ public class LocationFromBucketName implements Function<String, Location> {
 
    public Location apply(String bucket) {
       Set<? extends Location> locations = this.locations.get();
-      if (locations.size() == 1)
+      if (locations.size() == 1) {
          return get(locations, 0);
-      final Optional<String> region = bucketToRegion.apply(bucket);
-      if (region.isPresent()) {
-         try {
-            return find(locations, idEquals(region.get()));
-         } catch (NoSuchElementException e) {
-            logger.debug("could not get location for region %s in %s", region, locations);
-         }
-      } else {
-         logger.debug("could not get region for %s", bucket);
       }
+
+      Optional<String> region = bucketToRegion.apply(bucket);
+      return getLocationForRegion(locations, region, bucket);
+   }
+
+   private Location getLocationForRegion(Set<? extends Location> locations, Optional<String> region, String bucket) {
+      if (region.isPresent()) {
+         return findLocationByRegion(locations, region.get());
+      }
+
+      logger.debug("could not get region for %s", bucket);
       return null;
+   }
+
+   private Location findLocationByRegion(Set<? extends Location> locations, String region) {
+      try {
+         return find(locations, idEquals(region));
+      } catch (NoSuchElementException e) {
+         logger.debug("could not get location for region %s in %s", region, locations);
+         return null;
+      }
    }
 }
