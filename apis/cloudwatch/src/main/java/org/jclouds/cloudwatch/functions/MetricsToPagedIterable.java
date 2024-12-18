67221a44c6e066b2752227944855c81546fdcb66
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:27:08 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/functions/MetricsToPagedIterable.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/functions/MetricsToPagedIterable.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/functions/MetricsToPagedIterable.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -43,18 +28,26 @@ public class MetricsToPagedIterable extends CallerArg0ToPagedIterable<Metric, Me
    @Override
    protected Function<Object, IterableWithMarker<Metric>> markerToNextForCallingArg0(final String arg0) {
       final MetricApi metricApi = api.getMetricApiForRegion(arg0);
-      return new Function<Object, IterableWithMarker<Metric>>() {
-
-         @Override
-         public IterableWithMarker<Metric> apply(Object input) {
-            return metricApi.list(ListMetricsOptions.Builder.afterMarker(input.toString()));
-         }
-
-         @Override
-         public String toString() {
-            return "listMetricsInRegion(" + arg0 + ")";
-         }
-      };
+      return new MarkerToNextFunction(metricApi, arg0);
    }
 
+   private static class MarkerToNextFunction implements Function<Object, IterableWithMarker<Metric>> {
+      private final MetricApi metricApi;
+      private final String region;
+
+      MarkerToNextFunction(MetricApi metricApi, String region) {
+         this.metricApi = metricApi;
+         this.region = region;
+      }
+
+      @Override
+      public IterableWithMarker<Metric> apply(Object input) {
+         return metricApi.list(ListMetricsOptions.Builder.afterMarker(input.toString()));
+      }
+
+      @Override
+      public String toString() {
+         return "listMetricsInRegion(" + region + ")";
+      }
+   }
 }
