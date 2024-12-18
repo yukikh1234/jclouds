4015b8ff3365032c43a811be2b63572a85681219
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 16:55:03 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/internal/S3BlobStoreContextImpl.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/internal/S3BlobStoreContextImpl.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/internal/S3BlobStoreContextImpl.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.blobstore.internal;
 
 import jakarta.inject.Inject;
@@ -34,14 +19,21 @@ import com.google.common.reflect.TypeToken;
 @Singleton
 public class S3BlobStoreContextImpl extends BlobStoreContextImpl implements S3BlobStoreContext {
 
+   private final S3BlobStore s3BlobStore;
+
    @Inject
-   public S3BlobStoreContextImpl(@Provider Context backend, @Provider TypeToken<? extends Context> backendType,
-         Utils utils, ConsistencyModel consistencyModel, BlobStore blobStore, BlobRequestSigner blobRequestSigner) {
+   public S3BlobStoreContextImpl(@Provider Context backend, 
+                                 @Provider TypeToken<? extends Context> backendType,
+                                 Utils utils, 
+                                 ConsistencyModel consistencyModel, 
+                                 BlobStore blobStore, 
+                                 BlobRequestSigner blobRequestSigner) {
       super(backend, backendType, utils, consistencyModel, blobStore, blobRequestSigner);
+      this.s3BlobStore = S3BlobStore.class.cast(blobStore);
    }
 
    @Override
    public S3BlobStore getBlobStore() {
-      return S3BlobStore.class.cast(super.getBlobStore());
+      return s3BlobStore;
    }
 }
