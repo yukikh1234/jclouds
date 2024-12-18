f9359595ef2a76ac417acfe3c2e0401f9bf678e3
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 15:15:02 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/Dimension.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/Dimension.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/Dimension.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.domain;
 
 import com.google.common.base.MoreObjects;
@@ -30,6 +15,30 @@ public class Dimension {
    public Dimension(String name, String value) {
       this.name = name;
       this.value = value;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (!(obj instanceof Dimension)) {
+         return false;
+      }
+      Dimension other = (Dimension) obj;
+      return Objects.equal(this.name, other.name) &&
+             Objects.equal(this.value, other.value);
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public int hashCode() {
+      return Objects.hashCode(name, value);
    }
 
    /**
@@ -50,34 +59,9 @@ public class Dimension {
     * {@inheritDoc}
     */
    @Override
-   public int hashCode() {
-      return Objects.hashCode(name, value);
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      Dimension other = (Dimension)obj;
-      return Objects.equal(this.name, other.name) &&
-             Objects.equal(this.value, other.value);
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   @Override
    public String toString() {
       return MoreObjects.toStringHelper(this)
                     .add("name", name)
                     .add("value", value).toString();
    }
-
 }
