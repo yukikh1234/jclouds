33e299a61038608c862020987812dd6fb93f56e3
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:39:37 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/GetMetricStatisticsResponse.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/GetMetricStatisticsResponse.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/GetMetricStatisticsResponse.java
@@ -1,27 +1,12 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.domain;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 import org.jclouds.javax.annotation.Nullable;
 
 import com.google.common.base.MoreObjects;
-import com.google.common.base.Objects;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
 
@@ -32,16 +17,14 @@ import com.google.common.collect.ImmutableSet;
  */
 public class GetMetricStatisticsResponse extends FluentIterable<Datapoint> {
 
-   private final Iterable<Datapoint> datapoints;
+   private final ImmutableSet<Datapoint> datapoints;
    private final String label;
 
    public GetMetricStatisticsResponse(@Nullable Iterable<Datapoint> datapoints, String label) {
-      // Default to an empty set
-      if (datapoints == null) {
-         this.datapoints = ImmutableSet.<Datapoint>of();
-      } else {
-         this.datapoints = ImmutableSet.<Datapoint>copyOf(datapoints);
+      if (label == null || label.isEmpty()) {
+         throw new IllegalArgumentException("Label cannot be null or empty");
       }
+      this.datapoints = (datapoints == null) ? ImmutableSet.of() : ImmutableSet.copyOf(datapoints);
       this.label = label;
    }
 
@@ -57,23 +40,19 @@ public class GetMetricStatisticsResponse extends FluentIterable<Datapoint> {
     */
    @Override
    public int hashCode() {
-      return Objects.hashCode(datapoints, label);
+      return Objects.hash(datapoints, label);
    }
 
-    /**
+   /**
     * {@inheritDoc}
     */
    @Override
    public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      GetMetricStatisticsResponse other = (GetMetricStatisticsResponse)obj;
-      return Objects.equal(this.datapoints, other.datapoints) &&
-             Objects.equal(this.label, other.label);
+      if (this == obj) return true;
+      if (!(obj instanceof GetMetricStatisticsResponse)) return false;
+      GetMetricStatisticsResponse other = (GetMetricStatisticsResponse) obj;
+      return Objects.equals(this.datapoints, other.datapoints) &&
+             Objects.equals(this.label, other.label);
    }
 
    /**
@@ -82,13 +61,13 @@ public class GetMetricStatisticsResponse extends FluentIterable<Datapoint> {
    @Override
    public String toString() {
       return MoreObjects.toStringHelper(this)
-                    .add("label", label)
-                    .add("datapoints", datapoints).toString();
+                        .add("label", label)
+                        .add("datapoints", datapoints)
+                        .toString();
    }
 
    @Override
    public Iterator<Datapoint> iterator() {
       return datapoints.iterator();
    }
-
 }
