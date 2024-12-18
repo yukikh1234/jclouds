f765ab42a448a8149d974edbeab9d2e71a812e99
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 13:41:27 -0500

    Your changing file apis/byon/src/main/java/org/jclouds/byon/suppliers/NodesParsedFromSupplier.java

--- apis/byon/src/main/java/org/jclouds/byon/suppliers/NodesParsedFromSupplier.java
+++ apis/byon/src/main/java/org/jclouds/byon/suppliers/NodesParsedFromSupplier.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.byon.suppliers;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -35,22 +20,29 @@ import com.google.common.io.ByteSource;
 @Singleton
 public class NodesParsedFromSupplier implements Supplier<LoadingCache<String, Node>> {
    @Resource
-   protected Logger logger = Logger.NULL;
+   private Logger logger = Logger.NULL;
 
    private final ByteSource supplier;
    private final Function<ByteSource, LoadingCache<String, Node>> parser;
 
    @Inject
-   NodesParsedFromSupplier(@Provider ByteSource supplier, Function<ByteSource, LoadingCache<String, Node>> parser) {
+   public NodesParsedFromSupplier(@Provider ByteSource supplier, Function<ByteSource, LoadingCache<String, Node>> parser) {
       this.supplier = checkNotNull(supplier, "supplier");
       this.parser = checkNotNull(parser, "parser");
    }
 
    @Override
    public LoadingCache<String, Node> get() {
-      LoadingCache<String, Node> nodes = parser.apply(supplier);
-      checkState(nodes != null && nodes.size() > 0, "no nodes parsed from supplier: %s", supplier);
+      LoadingCache<String, Node> nodes = parseNodes();
+      validateNodes(nodes);
       return nodes;
    }
 
+   private LoadingCache<String, Node> parseNodes() {
+      return parser.apply(supplier);
+   }
+
+   private void validateNodes(LoadingCache<String, Node> nodes) {
+      checkState(nodes != null && nodes.size() > 0, "no nodes parsed from supplier: %s", supplier);
+   }
 }
