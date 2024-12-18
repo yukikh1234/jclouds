442ffc74a5b5c99a5e8c19a37dde64fb4c24debc
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:26:14 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/functions/ListAlarmHistoryToPagedIterable.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/functions/ListAlarmHistoryToPagedIterable.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/functions/ListAlarmHistoryToPagedIterable.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -41,18 +26,26 @@ public class ListAlarmHistoryToPagedIterable
    @Override
    protected Function<Object, IterableWithMarker<AlarmHistoryItem>> markerToNextForCallingArg0(final String arg0) {
       final AlarmApi alarmApi = api.getAlarmApiForRegion(arg0);
-      return new Function<Object, IterableWithMarker<AlarmHistoryItem>>() {
-
-         @Override
-         public IterableWithMarker<AlarmHistoryItem> apply(Object input) {
-            return alarmApi.listHistoryAt(input.toString());
-         }
-
-         @Override
-         public String toString() {
-            return "listHistory(" + arg0 + ")";
-         }
-      };
+      return new AlarmHistoryFunction(alarmApi, arg0);
    }
 
+   private static class AlarmHistoryFunction implements Function<Object, IterableWithMarker<AlarmHistoryItem>> {
+      private final AlarmApi alarmApi;
+      private final String region;
+
+      AlarmHistoryFunction(AlarmApi alarmApi, String region) {
+         this.alarmApi = alarmApi;
+         this.region = region;
+      }
+
+      @Override
+      public IterableWithMarker<AlarmHistoryItem> apply(Object input) {
+         return alarmApi.listHistoryAt(input.toString());
+      }
+
+      @Override
+      public String toString() {
+         return "listHistory(" + region + ")";
+      }
+   }
 }
