17f20002d117bdd1884431929382d33ea7eef11c
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:00:05 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/BucketToResourceMetadata.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/BucketToResourceMetadata.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/BucketToResourceMetadata.java
@@ -1,21 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package org.jclouds.s3.blobstore.functions;
 
+package org.jclouds.s3.blobstore.functions;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -34,11 +18,16 @@ public class BucketToResourceMetadata implements Function<BucketMetadata, Storag
    private final Function<String, Location> locationOfBucket;
 
    @Inject
-   BucketToResourceMetadata(Function<String, Location> locationOfBucket) {
+   public BucketToResourceMetadata(Function<String, Location> locationOfBucket) {
       this.locationOfBucket = locationOfBucket;
    }
 
+   @Override
    public StorageMetadata apply(BucketMetadata from) {
+      return createStorageMetadata(from);
+   }
+
+   private StorageMetadata createStorageMetadata(BucketMetadata from) {
       MutableStorageMetadata to = new MutableStorageMetadataImpl();
       to.setName(from.getName());
       to.setType(StorageType.CONTAINER);
