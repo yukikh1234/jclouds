3fadc301b72206f5769b60fef87b38b324483caf
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 15:45:12 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/filters/Aws4SignerForAuthorizationHeader.java

--- apis/s3/src/main/java/org/jclouds/s3/filters/Aws4SignerForAuthorizationHeader.java
+++ apis/s3/src/main/java/org/jclouds/s3/filters/Aws4SignerForAuthorizationHeader.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.filters;
 
 import com.google.common.base.Joiner;
@@ -58,6 +43,7 @@ import static org.jclouds.s3.reference.S3Constants.PROPERTY_S3_VIRTUAL_HOST_BUCK
  * AWS4 signer sign requests to Amazon S3 using an 'Authorization' header.
  */
 public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
+   
    @Inject
    public Aws4SignerForAuthorizationHeader(SignatureWire signatureWire,
          @Named(PROPERTY_S3_VIRTUAL_HOST_BUCKETS) boolean isVhostStyle,
@@ -68,12 +54,9 @@ public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
    }
 
    protected HttpRequest sign(HttpRequest request) throws HttpException {
-      checkNotNull(request, "request is not ready to sign");
-      checkNotNull(request.getEndpoint(), "request is not ready to sign, request.endpoint not present.");
+      validateRequest(request);
 
       Payload payload = request.getPayload();
-
-      // get host & port from request endpoint.
       String host = request.getEndpoint().getHost();
 
       Date date = timestampProvider.get();
@@ -84,28 +67,59 @@ public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
       String region = serviceAndRegion.region(host);
       String credentialScope = Joiner.on('/').join(datestamp, region, service, "aws4_request");
 
-      HttpRequest.Builder<?> requestBuilder = request.toBuilder() //
-            .removeHeader(AUTHORIZATION) // remove Authorization
-            .removeHeader(DATE); // remove date
+      HttpRequest.Builder<?> requestBuilder = prepareRequestBuilder(request, timestamp, host);
 
-      ImmutableMap.Builder<String, String> signedHeadersBuilder = ImmutableSortedMap.<String, String>naturalOrder();
+      ImmutableMap<String, String> signedHeaders = buildSignedHeaders(request, requestBuilder, payload, host);
 
-      // Content Type
-      // content-type is not a required signing param. However, examples use this, so we include it to ease testing.
-      String contentType = getContentType(request);
-      if (!Strings.isNullOrEmpty(contentType)) {
-         requestBuilder.replaceHeader(HttpHeaders.CONTENT_TYPE, contentType);
-         signedHeadersBuilder.put(HttpHeaders.CONTENT_TYPE.toLowerCase(), contentType);
+      String stringToSign = createStringToSign(request.getMethod(), request.getEndpoint(), signedHeaders, timestamp,
+            credentialScope, getPayloadHash(request));
+      signatureWire.getWireLog().debug("<< " + stringToSign);
+
+      byte[] signatureKey = signatureKey(creds.get().credential, datestamp, region, service);
+      String signature = base16().lowerCase().encode(hmacSHA256(stringToSign, signatureKey));
+
+      String authorizationHeader = buildAuthorizationHeader(creds.get(), credentialScope, signedHeaders, signature);
+      return requestBuilder.replaceHeader(HttpHeaders.AUTHORIZATION, authorizationHeader).build();
+   }
+
+   private void validateRequest(HttpRequest request) {
+      checkNotNull(request, "request is not ready to sign");
+      checkNotNull(request.getEndpoint(), "request is not ready to sign, request.endpoint not present.");
+   }
+
+   private HttpRequest.Builder<?> prepareRequestBuilder(HttpRequest request, String timestamp, String host) {
+      return request.toBuilder()
+            .removeHeader(AUTHORIZATION)
+            .removeHeader(DATE)
+            .replaceHeader(AMZ_DATE_HEADER, timestamp)
+            .replaceHeader(HttpHeaders.HOST, hostHeaderFor(request.getEndpoint()));
+   }
+
+   private ImmutableMap<String, String> buildSignedHeaders(HttpRequest request, HttpRequest.Builder<?> requestBuilder, Payload payload, String host) {
+      ImmutableMap.Builder<String, String> signedHeadersBuilder = ImmutableSortedMap.naturalOrder();
+
+      addHeaderIfNotEmpty(requestBuilder, signedHeadersBuilder, HttpHeaders.CONTENT_TYPE, getContentType(request));
+      addHeaderIfNotEmpty(requestBuilder, signedHeadersBuilder, HttpHeaders.CONTENT_LENGTH, getContentLength(request));
+      addHeaderIfNotEmpty(requestBuilder, signedHeadersBuilder, CONTENT_MD5, getContentMD5(request, payload));
+      addHeaderIfNotEmpty(requestBuilder, signedHeadersBuilder, HttpHeaders.USER_AGENT, request.getFirstHeaderOrNull(HttpHeaders.USER_AGENT));
+
+      appendAmzHeaders(request, signedHeadersBuilder);
+      addSessionToken(requestBuilder, signedHeadersBuilder);
+      String contentSha256 = getPayloadHash(request);
+      requestBuilder.replaceHeader(AMZ_CONTENT_SHA256_HEADER, contentSha256);
+      signedHeadersBuilder.put(AMZ_CONTENT_SHA256_HEADER.toLowerCase(), contentSha256);
+
+      return signedHeadersBuilder.build();
+   }
+
+   private void addHeaderIfNotEmpty(HttpRequest.Builder<?> requestBuilder, ImmutableMap.Builder<String, String> signedHeadersBuilder, String header, String value) {
+      if (!Strings.isNullOrEmpty(value)) {
+         requestBuilder.replaceHeader(header, value);
+         signedHeadersBuilder.put(header.toLowerCase(), value);
       }
+   }
 
-      // Content-Length for PUT or POST request http method
-      String contentLength = getContentLength(request);
-      if (!Strings.isNullOrEmpty(contentLength)) {
-         requestBuilder.replaceHeader(HttpHeaders.CONTENT_LENGTH, contentLength);
-         signedHeadersBuilder.put(HttpHeaders.CONTENT_LENGTH.toLowerCase(), contentLength);
-      }
-
-      // Content MD5
+   private String getContentMD5(HttpRequest request, Payload payload) {
       String contentMD5 = request.getFirstHeaderOrNull(CONTENT_MD5);
       if (payload != null) {
          HashCode md5 = payload.getContentMetadata().getContentMD5AsHashCode();
@@ -113,58 +127,24 @@ public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
             contentMD5 = BaseEncoding.base64().encode(md5.asBytes());
          }
       }
-      if (contentMD5 != null) {
-         requestBuilder.replaceHeader(CONTENT_MD5, contentMD5);
-         signedHeadersBuilder.put(CONTENT_MD5.toLowerCase(), contentMD5);
-      }
+      return contentMD5;
+   }
 
-      // host
-      host = hostHeaderFor(request.getEndpoint());
-      requestBuilder.replaceHeader(HttpHeaders.HOST, host);
-      signedHeadersBuilder.put(HttpHeaders.HOST.toLowerCase(), host);
-
-      // user-agent
-      String userAgent = request.getFirstHeaderOrNull(HttpHeaders.USER_AGENT);
-      if (userAgent != null) {
-         signedHeadersBuilder.put(HttpHeaders.USER_AGENT.toLowerCase(), userAgent);
-      }
-
-      // all x-amz-* headers
-      appendAmzHeaders(request, signedHeadersBuilder);
-
-      // x-amz-security-token
+   private void addSessionToken(HttpRequest.Builder<?> requestBuilder, ImmutableMap.Builder<String, String> signedHeadersBuilder) {
       Credentials credentials = creds.get();
       if (credentials instanceof SessionCredentials) {
-         String token = SessionCredentials.class.cast(credentials).getSessionToken();
+         String token = ((SessionCredentials) credentials).getSessionToken();
          requestBuilder.replaceHeader(AMZ_SECURITY_TOKEN_HEADER, token);
          signedHeadersBuilder.put(AMZ_SECURITY_TOKEN_HEADER.toLowerCase(), token);
       }
+   }
 
-      // x-amz-content-sha256
-      String contentSha256 = getPayloadHash(request);
-      requestBuilder.replaceHeader(AMZ_CONTENT_SHA256_HEADER, contentSha256);
-      signedHeadersBuilder.put(AMZ_CONTENT_SHA256_HEADER.toLowerCase(), contentSha256);
-
-      // put x-amz-date
-      requestBuilder.replaceHeader(AMZ_DATE_HEADER, timestamp);
-      signedHeadersBuilder.put(AMZ_DATE_HEADER.toLowerCase(), timestamp);
-
-      ImmutableMap<String, String> signedHeaders = signedHeadersBuilder.build();
-
-      String stringToSign = createStringToSign(request.getMethod(), request.getEndpoint(), signedHeaders, timestamp,
-            credentialScope, contentSha256);
-      signatureWire.getWireLog().debug("<< " + stringToSign);
-
-      byte[] signatureKey = signatureKey(credentials.credential, datestamp, region, service);
-      String signature = base16().lowerCase().encode(hmacSHA256(stringToSign, signatureKey));
-
-      StringBuilder authorization = new StringBuilder(AMZ_ALGORITHM_HMAC_SHA256).append(" ");
-      authorization.append("Credential=").append(Joiner.on("/").join(credentials.identity, credentialScope))
-            .append(", ");
-      authorization.append("SignedHeaders=").append(Joiner.on(";").join(signedHeaders.keySet()))
-            .append(", ");
-      authorization.append("Signature=").append(signature);
-      return requestBuilder.replaceHeader(HttpHeaders.AUTHORIZATION, authorization.toString()).build();
+   private String buildAuthorizationHeader(Credentials credentials, String credentialScope, ImmutableMap<String, String> signedHeaders, String signature) {
+      return new StringBuilder(AMZ_ALGORITHM_HMAC_SHA256)
+            .append(" Credential=").append(Joiner.on("/").join(credentials.identity, credentialScope))
+            .append(", SignedHeaders=").append(Joiner.on(";").join(signedHeaders.keySet()))
+            .append(", Signature=").append(signature)
+            .toString();
    }
 
    protected String getPayloadHash(HttpRequest request) {
@@ -175,46 +155,27 @@ public class Aws4SignerForAuthorizationHeader extends Aws4SignerBase {
       return calculatePayloadContentHash(payload);
    }
 
-   /**
-    * The hash returns the following value: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-    */
    protected String getEmptyPayloadContentHash() {
       return base16().lowerCase().encode(hash(new ByteArrayInputStream(new byte[0])));
    }
 
-   /**
-    * in this time, payload ContentMetadata provided content hash md5, but aws required sha256.
-    */
    protected String calculatePayloadContentHash(Payload payload) {
-      // use payload stream calculate content sha256
-      InputStream payloadStream;
-      try {
-         payloadStream = payload.openStream();
+      try (InputStream payloadStream = payload.openStream()) {
+         return base16().lowerCase().encode(hash(payloadStream));
       } catch (IOException e) {
          throw new HttpException("unable to open payload stream to calculate AWS4 signature.", e);
       }
-      try {
-         return base16().lowerCase().encode(hash(payloadStream));
-      } finally {
-         closeOrResetPayloadStream(payloadStream, payload.isRepeatable());
-      }
    }
 
-   // some times, when use Multipart Payload and a part can not be repeatable, will happen some error...
    void closeOrResetPayloadStream(InputStream payloadStream, boolean repeatable) {
-      // if payload stream can repeatable.
-      if (repeatable) {
-         Closeables2.closeQuietly(payloadStream);
-      } else {
-         try {
-            // reset unrepeatable payload stream
+      try {
+         if (repeatable) {
+            Closeables2.closeQuietly(payloadStream);
+         } else {
             payloadStream.reset();
-         } catch (IOException e) {
-            // reset payload stream
-            throw new HttpException(
-                  "unable to reset unrepeatable payload stream after calculating AWS4 signature.", e);
          }
+      } catch (IOException e) {
+         throw new HttpException("unable to reset unrepeatable payload stream after calculating AWS4 signature.", e);
       }
    }
-
 }
