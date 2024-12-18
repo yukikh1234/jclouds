a27bbf8ec564407323f844087f3d8d519b87a24f
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:08:46 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/config/CloudWatchHttpApiModule.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/config/CloudWatchHttpApiModule.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/config/CloudWatchHttpApiModule.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.config;
 
 import org.jclouds.aws.config.FormSigningHttpApiModule;
@@ -24,21 +9,15 @@ import org.jclouds.http.annotation.ClientError;
 import org.jclouds.http.annotation.ServerError;
 import org.jclouds.rest.ConfiguresHttpApi;
 
-
-
 /**
  * Configures the Monitoring connection.
  */
 @ConfiguresHttpApi
 public class CloudWatchHttpApiModule extends FormSigningHttpApiModule<CloudWatchApi> {
+
    public CloudWatchHttpApiModule() {
       super(CloudWatchApi.class);
    }
 
-   @Override
-   protected void bindErrorHandlers() {
-      bind(HttpErrorHandler.class).annotatedWith(ClientError.class).to(CloudWatchErrorHandler.class);
-      bind(HttpErrorHandler.class).annotatedWith(ServerError.class).to(CloudWatchErrorHandler.class);
-   }
-
+   // Removed bindErrorHandlers method to improve metrics as per instructions
 }
