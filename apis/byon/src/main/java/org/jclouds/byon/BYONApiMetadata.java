05b927f02f624fe1e34ed18ce2087a783ba14121
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 13:40:34 -0500

    Your changing file apis/byon/src/main/java/org/jclouds/byon/BYONApiMetadata.java

--- apis/byon/src/main/java/org/jclouds/byon/BYONApiMetadata.java
+++ apis/byon/src/main/java/org/jclouds/byon/BYONApiMetadata.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.byon;
 
 import java.net.URI;
@@ -56,17 +41,29 @@ public class BYONApiMetadata extends BaseApiMetadata {
    public static class Builder extends BaseApiMetadata.Builder<Builder> {
 
       protected Builder() {
-         id("byon")
-         .name("Bring Your Own Node (BYON) API")
-         .identityName("Unused")
-         .defaultIdentity("foo")
-         .defaultCredential("bar")
-         .defaultEndpoint("file://byon.yaml")
-         .documentation(URI.create("https://github.com/jclouds/jclouds/tree/master/apis/byon"))
-         .version(String.format("%s.%s", JcloudsVersion.get().majorVersion, JcloudsVersion.get().minorVersion))
-         .buildVersion(JcloudsVersion.get().toString())
-         .view(ComputeServiceContext.class)
-         .defaultModules(ImmutableSet.<Class<? extends Module>>of(YamlNodeStoreModule.class, BYONComputeServiceContextModule.class));
+         initBuilder();
+      }
+      
+      private void initBuilder() {
+          id("byon")
+          .name("Bring Your Own Node (BYON) API")
+          .identityName("Unused")
+          .defaultIdentity("foo")
+          .defaultCredential("bar")
+          .defaultEndpoint("file://byon.yaml")
+          .documentation(URI.create("https://github.com/jclouds/jclouds/tree/master/apis/byon"))
+          .version(getVersion())
+          .buildVersion(JcloudsVersion.get().toString())
+          .view(ComputeServiceContext.class)
+          .defaultModules(getDefaultModules());
+      }
+
+      private String getVersion() {
+          return String.format("%s.%s", JcloudsVersion.get().majorVersion, JcloudsVersion.get().minorVersion);
+      }
+      
+      private ImmutableSet<Class<? extends Module>> getDefaultModules() {
+          return ImmutableSet.of(YamlNodeStoreModule.class, BYONComputeServiceContextModule.class);
       }
 
       @Override
