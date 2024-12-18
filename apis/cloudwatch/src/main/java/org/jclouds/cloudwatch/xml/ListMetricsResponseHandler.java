d9a4e955553205ab4a17da8ac6fcd034105e14d1
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:18:48 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListMetricsResponseHandler.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListMetricsResponseHandler.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/xml/ListMetricsResponseHandler.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.xml;
 
 import java.util.Set;
@@ -35,7 +20,6 @@ import com.google.inject.Inject;
 public class ListMetricsResponseHandler extends ParseSax.HandlerForGeneratedRequestWithResult<IterableWithMarker<Metric>> {
 
    private final MetricHandler metricHandler;
-
    private StringBuilder currentText = new StringBuilder();
    private Set<Metric> metrics = Sets.newLinkedHashSet();
    private boolean inMetrics;
@@ -46,50 +30,31 @@ public class ListMetricsResponseHandler extends ParseSax.HandlerForGeneratedRequ
       this.metricHandler = metricHandler;
    }
 
-   /**
-    * {@inheritDoc}
-    */
    @Override
    public IterableWithMarker<Metric> getResult() {
       return IterableWithMarkers.from(metrics, nextToken);
    }
 
-   /**
-    * {@inheritDoc}
-    */
    @Override
    public void startElement(String url, String name, String qName, Attributes attributes) throws SAXException {
       if (SaxUtils.equalsOrSuffix(qName, "Metrics")) {
-         inMetrics = true;
+         handleStartMetrics();
       }
       if (inMetrics) {
          metricHandler.startElement(url, name, qName, attributes);
       }
    }
 
-   /**
-    * {@inheritDoc}
-    */
    @Override
    public void endElement(String uri, String name, String qName) throws SAXException {
       if (inMetrics) {
-         if (qName.equals("Metrics")) {
-            inMetrics = false;
-         } else if (qName.equals("member") && !metricHandler.inDimensions()) {
-            metrics.add(metricHandler.getResult());
-         } else {
-            metricHandler.endElement(uri, name, qName);
-         }
+         handleEndMetrics(uri, name, qName);
       } else if (qName.equals("NextToken")) {
-         nextToken = SaxUtils.currentOrNull(currentText);
+         handleEndNextToken();
       }
-
       currentText.setLength(0);
    }
 
-   /**
-    * {@inheritDoc}
-    */
    @Override
    public void characters(char[] ch, int start, int length) {
       if (inMetrics) {
@@ -99,4 +64,21 @@ public class ListMetricsResponseHandler extends ParseSax.HandlerForGeneratedRequ
       }
    }
 
+   private void handleStartMetrics() {
+      inMetrics = true;
+   }
+
+   private void handleEndMetrics(String uri, String name, String qName) throws SAXException {
+      if (qName.equals("Metrics")) {
+         inMetrics = false;
+      } else if (qName.equals("member") && !metricHandler.inDimensions()) {
+         metrics.add(metricHandler.getResult());
+      } else {
+         metricHandler.endElement(uri, name, qName);
+      }
+   }
+
+   private void handleEndNextToken() {
+      nextToken = SaxUtils.currentOrNull(currentText);
+   }
 }
