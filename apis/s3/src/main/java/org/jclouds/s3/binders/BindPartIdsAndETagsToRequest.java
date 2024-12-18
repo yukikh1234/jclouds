fb44c81e0d6d8eac322b6aa91fb3d3b4665bc734
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:32:48 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/binders/BindPartIdsAndETagsToRequest.java

--- apis/s3/src/main/java/org/jclouds/s3/binders/BindPartIdsAndETagsToRequest.java
+++ apis/s3/src/main/java/org/jclouds/s3/binders/BindPartIdsAndETagsToRequest.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.binders;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -33,14 +18,24 @@ import org.jclouds.rest.Binder;
 @Singleton
 public class BindPartIdsAndETagsToRequest implements Binder {
 
-   @SuppressWarnings("unchecked")
    @Override
    public <R extends HttpRequest> R bindToRequest(R request, Object input) {
-      checkArgument(checkNotNull(input, "input") instanceof Map, "this binder is only valid for Map!");
-      checkNotNull(request, "request");
+      validateInput(request, input);
+      Map<Integer, String> partsMap = (Map<Integer, String>) input;
+      String xmlContent = createMultipartUploadXml(partsMap);
+      setPayload(request, xmlContent);
+      return request;
+   }
 
-      Map<Integer, String> map = (Map<Integer, String>) input;
+   private <R extends HttpRequest> void validateInput(R request, Object input) {
+      checkNotNull(request, "request");
+      checkNotNull(input, "input");
+      checkArgument(input instanceof Map, "this binder is only valid for Map!");
+      Map<?, ?> map = (Map<?, ?>) input;
       checkArgument(!map.isEmpty(), "Please send parts");
+   }
+
+   private String createMultipartUploadXml(Map<Integer, String> map) {
       StringBuilder content = new StringBuilder();
       content.append("<CompleteMultipartUpload>");
       for (Entry<Integer, String> entry : map.entrySet()) {
@@ -50,9 +45,12 @@ public class BindPartIdsAndETagsToRequest implements Binder {
          content.append("</Part>");
       }
       content.append("</CompleteMultipartUpload>");
-      Payload payload = Payloads.newStringPayload(content.toString());
+      return content.toString();
+   }
+
+   private <R extends HttpRequest> void setPayload(R request, String xmlContent) {
+      Payload payload = Payloads.newStringPayload(xmlContent);
       payload.getContentMetadata().setContentType(MediaType.TEXT_XML);
       request.setPayload(payload);
-      return request;
    }
 }
