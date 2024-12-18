3f1b249322237c6255b60bb607356a09327f5a24
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:59:13 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/domain/BucketMetadata.java

--- apis/s3/src/main/java/org/jclouds/s3/domain/BucketMetadata.java
+++ apis/s3/src/main/java/org/jclouds/s3/domain/BucketMetadata.java
@@ -1,22 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.domain;
 
 import java.util.Date;
+import java.util.Objects;
 
 /**
  * System metadata of the S3Bucket
@@ -66,32 +52,18 @@ public class BucketMetadata implements Comparable<BucketMetadata> {
 
    @Override
    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((name == null) ? 0 : name.hashCode());
-      result = prime * result + ((owner == null) ? 0 : owner.hashCode());
-      return result;
+      return Objects.hash(name, owner);
    }
 
    @Override
    public boolean equals(Object obj) {
-      if (this == obj)
+      if (this == obj) {
          return true;
-      if (obj == null)
+      }
+      if (obj == null || getClass() != obj.getClass()) {
          return false;
-      if (getClass() != obj.getClass())
-         return false;
+      }
       BucketMetadata other = (BucketMetadata) obj;
-      if (name == null) {
-         if (other.name != null)
-            return false;
-      } else if (!name.equals(other.name))
-         return false;
-      if (owner == null) {
-         if (other.owner != null)
-            return false;
-      } else if (!owner.equals(other.owner))
-         return false;
-      return true;
+      return Objects.equals(name, other.name) && Objects.equals(owner, other.owner);
    }
 }
