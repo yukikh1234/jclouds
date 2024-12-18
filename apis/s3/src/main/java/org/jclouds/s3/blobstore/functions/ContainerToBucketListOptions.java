a295f332bd58ce042a796c6479046be801641045
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:04:55 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/ContainerToBucketListOptions.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/ContainerToBucketListOptions.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/ContainerToBucketListOptions.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.blobstore.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -28,41 +13,55 @@ import com.google.common.base.Function;
 @Singleton
 public class ContainerToBucketListOptions implements
          Function<ListContainerOptions, ListBucketOptions> {
+
    public ListBucketOptions apply(ListContainerOptions from) {
       checkNotNull(from, "set options to instance NONE instead of passing null");
+      validateOptions(from);
+
+      ListBucketOptions httpOptions = new ListBucketOptions();
+      setDelimiter(from, httpOptions);
+      setPrefix(from, httpOptions);
+      setMarker(from, httpOptions);
+      setMaxResults(from, httpOptions);
+
+      return httpOptions;
+   }
+
+   private void validateOptions(ListContainerOptions from) {
       if (from.getPrefix() != null && from.getDir() != null) {
          throw new IllegalArgumentException("Cannot set both directory and prefix options");
       }
+   }
 
-      ListBucketOptions httpOptions = new ListBucketOptions();
+   private void setDelimiter(ListContainerOptions from, ListBucketOptions httpOptions) {
       if (!from.isRecursive()) {
-         if (from.getDelimiter() != null) {
-            httpOptions.delimiter(from.getDelimiter().toString());
-         } else {
-            httpOptions.delimiter("/");
-         }
+         String delimiter = from.getDelimiter() != null ? from.getDelimiter().toString() : "/";
+         httpOptions.delimiter(delimiter);
       }
-      if (from.getDir() != null) {// TODO unit test
+   }
+
+   private void setPrefix(ListContainerOptions from, ListBucketOptions httpOptions) {
+      if (from.getDir() != null) {
          String path = from.getDir();
-         if (from.getDelimiter() != null) {
-            if (!path.endsWith(from.getDelimiter().toString())) {
-               path += from.getDelimiter();
-            }
-         } else {
-            if (!path.endsWith("/"))
-               path = path + "/";
+         String delimiter = from.getDelimiter() != null ? from.getDelimiter().toString() : "/";
+         if (!path.endsWith(delimiter)) {
+            path += delimiter;
          }
          httpOptions.withPrefix(path);
-      }
-      if (from.getPrefix() != null) {
+      } else if (from.getPrefix() != null) {
          httpOptions.withPrefix(from.getPrefix());
       }
+   }
+
+   private void setMarker(ListContainerOptions from, ListBucketOptions httpOptions) {
       if (from.getMarker() != null) {
          httpOptions.afterMarker(from.getMarker());
       }
+   }
+
+   private void setMaxResults(ListContainerOptions from, ListBucketOptions httpOptions) {
       if (from.getMaxResults() != null) {
          httpOptions.maxResults(from.getMaxResults());
       }
-      return httpOptions;
    }
 }
