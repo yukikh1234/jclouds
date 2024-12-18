0861566da67fcd7826f505acd76a2d7cae666f22
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 14:43:59 -0500

    Your changing file apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/Datapoint.java

--- apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/Datapoint.java
+++ apis/cloudwatch/src/main/java/org/jclouds/cloudwatch/domain/Datapoint.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.cloudwatch.domain;
 
 import com.google.common.base.MoreObjects;
@@ -50,65 +35,41 @@ public class Datapoint {
       this.customUnit = customUnit;
    }
 
-   /**
-    * return Average of samples for the datapoint.
-    */
    @Nullable
    public Double getAverage() {
       return average;
    }
 
-   /**
-    * return Maximum of the samples used for the datapoint.
-    */
    @Nullable
    public Double getMaximum() {
       return maximum;
    }
 
-   /**
-    * return Minimum of samples for the datapoint.
-    */
    @Nullable
    public Double getMinimum() {
       return minimum;
    }
 
-   /**
-    * return Indicates the beginning of the time aggregation for this value and samples.
-    */
    @Nullable
    public Date getTimestamp() {
       return timestamp;
    }
 
-   /**
-    * return The number of Measurements that contributed to the aggregate value of this datapoint.
-    */
    @Nullable
    public Double getSamples() {
       return samples;
    }
 
-   /**
-    * return Sum of samples for the datapoint.
-    */
    @Nullable
    public Double getSum() {
       return sum;
    }
 
-   /**
-    * return Standard unit used for the datapoint.
-    */
    @Nullable
    public Unit getUnit() {
       return unit;
    }
 
-   /**
-    * return CustomUnit defined for the datapoint.
-    */
    @Nullable
    public String getCustomUnit() {
       return customUnit;
@@ -121,21 +82,17 @@ public class Datapoint {
 
    @Override
    public boolean equals(Object obj) {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
+      if (this == obj) return true;
+      if (!(obj instanceof Datapoint)) return false;
       Datapoint other = (Datapoint) obj;
-      return Objects.equal(this.average, other.average) &&
-             Objects.equal(this.customUnit, other.customUnit) &&
-             Objects.equal(this.maximum, other.maximum) &&
-             Objects.equal(this.minimum, other.minimum) &&
-             Objects.equal(this.samples, other.samples) &&
-             Objects.equal(this.sum, other.sum) &&
-             Objects.equal(this.timestamp, other.timestamp) &&
-             Objects.equal(this.unit, other.unit);
+      return Objects.equal(average, other.average) &&
+             Objects.equal(customUnit, other.customUnit) &&
+             Objects.equal(maximum, other.maximum) &&
+             Objects.equal(minimum, other.minimum) &&
+             Objects.equal(samples, other.samples) &&
+             Objects.equal(sum, other.sum) &&
+             Objects.equal(timestamp, other.timestamp) &&
+             Objects.equal(unit, other.unit);
    }
 
    @Override
@@ -150,5 +107,4 @@ public class Datapoint {
                     .add("samples", samples)
                     .add("unit", unit).toString();
    }
-
 }
