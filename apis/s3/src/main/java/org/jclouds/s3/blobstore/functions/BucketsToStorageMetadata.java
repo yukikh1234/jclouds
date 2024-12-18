07bb9c10e9f9c30900d14e0f43a3830827817aed
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 16:59:12 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/BucketsToStorageMetadata.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/BucketsToStorageMetadata.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/BucketsToStorageMetadata.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.blobstore.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -54,31 +39,37 @@ public class BucketsToStorageMetadata implements
       this.bucket2ResourceMd = checkNotNull(bucket2ResourceMd, "bucket2ResourceMd");
    }
 
-
    @Override
    public PageSet<? extends StorageMetadata> apply(Set<BucketMetadata> input) {
-      // parallel as listing buckets is slow when looking up regions
       Iterable<? extends StorageMetadata> buckets = FutureIterables
                .<BucketMetadata, StorageMetadata> transformParallel(input,
-                        new Function<BucketMetadata, ListenableFuture<? extends StorageMetadata>>() {
-                           @Override
-                           public ListenableFuture<? extends StorageMetadata> apply(final BucketMetadata from) {
-                              return userExecutor.submit(new Callable<StorageMetadata>() {
-
-                                 @Override
-                                 public StorageMetadata call() throws Exception {
-                                    return bucket2ResourceMd.apply(from);
-                                 }
-
-                                 @Override
-                                 public String toString() {
-                                    return "bucket2ResourceMd.apply(" + from + ")";
-                                 }
-                              });
-                           }
-
-                        }, userExecutor, null, logger, "my buckets");
+                        new BucketMetadataToStorageMetadataFunction(), userExecutor, null, logger, "my buckets");
       return new PageSetImpl<StorageMetadata>(buckets, null);
    }
 
+   private class BucketMetadataToStorageMetadataFunction 
+         implements Function<BucketMetadata, ListenableFuture<? extends StorageMetadata>> {
+      @Override
+      public ListenableFuture<? extends StorageMetadata> apply(final BucketMetadata from) {
+         return userExecutor.submit(new BucketMetadataCallable(from));
+      }
+   }
+
+   private class BucketMetadataCallable implements Callable<StorageMetadata> {
+      private final BucketMetadata from;
+
+      BucketMetadataCallable(BucketMetadata from) {
+         this.from = from;
+      }
+
+      @Override
+      public StorageMetadata call() throws Exception {
+         return bucket2ResourceMd.apply(from);
+      }
+
+      @Override
+      public String toString() {
+         return "bucket2ResourceMd.apply(" + from + ")";
+      }
+   }
 }
