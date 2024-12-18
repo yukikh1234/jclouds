ad85cd21b3a447e9b4836b196fe2bd7739999d1d
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:02:00 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/ObjectToBlob.java

--- apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/ObjectToBlob.java
+++ apis/s3/src/main/java/org/jclouds/s3/blobstore/functions/ObjectToBlob.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.blobstore.functions;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -34,15 +19,23 @@ public class ObjectToBlob implements Function<S3Object, Blob> {
 
    @Inject
    ObjectToBlob(Factory blobFactory, ObjectToBlobMetadata object2BlobMd) {
-      this.blobFactory = blobFactory;
-      this.object2BlobMd = object2BlobMd;
+      this.blobFactory = checkNotNull(blobFactory, "blobFactory");
+      this.object2BlobMd = checkNotNull(object2BlobMd, "object2BlobMd");
    }
 
    public Blob apply(S3Object from) {
-      if (from == null)
-         return null;
+      validateInput(from);
+      return createBlob(from);
+   }
+
+   private void validateInput(S3Object from) {
+      checkNotNull(from, "S3Object cannot be null");
+      checkNotNull(from.getPayload(), "payload: " + from);
+   }
+
+   private Blob createBlob(S3Object from) {
       Blob blob = blobFactory.create(object2BlobMd.apply(from.getMetadata()));
-      blob.setPayload(checkNotNull(from.getPayload(), "payload: " + from));
+      blob.setPayload(from.getPayload());
       blob.setAllHeaders(from.getAllHeaders());
       return blob;
    }
