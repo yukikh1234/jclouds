f8605a30ed86f225dbfac023bced66f1a92027ea
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:42:09 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/functions/DefaultEndpointThenInvalidateRegion.java

--- apis/s3/src/main/java/org/jclouds/s3/functions/DefaultEndpointThenInvalidateRegion.java
+++ apis/s3/src/main/java/org/jclouds/s3/functions/DefaultEndpointThenInvalidateRegion.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.functions;
 
 import java.net.URI;
@@ -33,7 +18,7 @@ public final class DefaultEndpointThenInvalidateRegion implements Function<Objec
    private final LoadingCache<String, Optional<String>> bucketToRegionCache;
 
    @Inject
-   DefaultEndpointThenInvalidateRegion(AssignCorrectHostnameForBucket delegate,
+   public DefaultEndpointThenInvalidateRegion(AssignCorrectHostnameForBucket delegate,
          @Bucket LoadingCache<String, Optional<String>> bucketToRegionCache) {
       this.delegate = delegate;
       this.bucketToRegionCache = bucketToRegionCache;
@@ -41,10 +26,8 @@ public final class DefaultEndpointThenInvalidateRegion implements Function<Objec
 
    @Override
    public URI apply(@Nullable Object from) {
-      try {
-         return delegate.apply(from);
-      } finally {
-         bucketToRegionCache.invalidate(from.toString());
-      }
+      URI result = delegate.apply(from);
+      bucketToRegionCache.invalidate(from.toString());
+      return result;
    }
 }
