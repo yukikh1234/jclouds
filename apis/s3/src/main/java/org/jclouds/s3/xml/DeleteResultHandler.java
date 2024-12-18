01ba416068eb246ead544a88b2113ab78569fb21
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:13:01 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/xml/DeleteResultHandler.java

--- apis/s3/src/main/java/org/jclouds/s3/xml/DeleteResultHandler.java
+++ apis/s3/src/main/java/org/jclouds/s3/xml/DeleteResultHandler.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.xml;
 
 import static org.jclouds.util.SaxUtils.equalsOrSuffix;
@@ -54,15 +39,15 @@ public class DeleteResultHandler extends ParseSax.HandlerForGeneratedRequestWith
    @Override
    public void startElement(String uri, String name, String qName, Attributes attributes)
       throws SAXException {
+      handleStartElement(qName, uri, name, attributes);
+   }
+
+   private void handleStartElement(String qName, String uri, String name, Attributes attributes) throws SAXException {
       if (equalsOrSuffix(qName, DELETED_TAG)) {
          parsingDeletedEntry = true;
+         deletedEntryAccumulator.setLength(0);
       } else if (equalsOrSuffix(qName, ERROR_TAG)) {
          parsingErrorEntry = true;
-      }
-
-      if (parsingDeletedEntry) {
-         deletedEntryAccumulator.setLength(0);
-      } else if (parsingErrorEntry) {
          errorEntryHandler.startElement(uri, name, qName, attributes);
       }
    }
@@ -81,6 +66,10 @@ public class DeleteResultHandler extends ParseSax.HandlerForGeneratedRequestWith
     */
    @Override
    public void endElement(String uri, String name, String qName) throws SAXException {
+      handleEndElement(qName, uri, name);
+   }
+
+   private void handleEndElement(String qName, String uri, String name) throws SAXException {
       if (equalsOrSuffix(qName, DELETED_TAG)) {
          parsingDeletedEntry = false;
          deleted.add(deletedEntryAccumulator.toString().trim());
