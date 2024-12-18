8b086934c1a5d81b65600dbdfdcf5527852fd1ce
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 17:57:58 -0500

    Your changing file apis/s3/src/main/java/org/jclouds/s3/domain/ListMultipartUploadResponse.java

--- apis/s3/src/main/java/org/jclouds/s3/domain/ListMultipartUploadResponse.java
+++ apis/s3/src/main/java/org/jclouds/s3/domain/ListMultipartUploadResponse.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.s3.domain;
 
 import static com.google.common.base.Preconditions.checkArgument;
@@ -34,12 +19,19 @@ public abstract class ListMultipartUploadResponse {
    public abstract long size();
 
    public static ListMultipartUploadResponse create(int partNumber, @Nullable Date lastModified, String eTag, long size) {
+      validateArguments(partNumber, eTag, size);
+      Date clonedLastModified = cloneDate(lastModified);
+      return new AutoValue_ListMultipartUploadResponse(partNumber, clonedLastModified, eTag, size);
+   }
+
+   private static void validateArguments(int partNumber, String eTag, long size) {
       checkArgument(partNumber > 0, "partNumber must be greater than zero, was: %s", partNumber);
       checkNotNull(eTag, "eTag");
-      if (lastModified != null) {
-         lastModified = (Date) lastModified.clone();
-      }
       checkArgument(size >= 0, "size must be positive, was: %s", size);
-      return new AutoValue_ListMultipartUploadResponse(partNumber, lastModified, eTag, size);
+   }
+
+   @Nullable
+   private static Date cloneDate(@Nullable Date date) {
+      return date != null ? (Date) date.clone() : null;
    }
 }
