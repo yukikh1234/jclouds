231b01237007eb016ae0bd6ccc288b18bfb87f04
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 19:07:38 -0500

    Your changing file apis/openstack-keystone/src/main/java/org/jclouds/openstack/v2_0/domain/Extension.java

--- apis/openstack-keystone/src/main/java/org/jclouds/openstack/v2_0/domain/Extension.java
+++ apis/openstack-keystone/src/main/java/org/jclouds/openstack/v2_0/domain/Extension.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.openstack.v2_0.domain;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -24,27 +9,16 @@ import java.util.Date;
 import java.util.Set;
 
 import org.jclouds.javax.annotation.Nullable;
-
 import com.google.common.base.Objects;
 import com.google.common.base.MoreObjects.ToStringHelper;
 
-/**
- * The OpenStack Compute API is extensible. Extensions serve two purposes: They
- * allow the introduction of new features in the API without requiring a version
- * change and they allow the introduction of vendor specific niche
- * functionality.
- * 
- * @see <a href=
-      "http://docs.openstack.org/api/openstack-compute/2/content/Extensions-d1e1444.html"
-      />
-*/
 public class Extension extends Resource {
 
-   public static Builder<?> builder() { 
+   public static Builder builder() { 
       return new ConcreteBuilder();
    }
    
-   public Builder<?> toBuilder() { 
+   public Builder toBuilder() { 
       return new ConcreteBuilder().fromExtension(this);
    }
 
@@ -54,41 +28,26 @@ public class Extension extends Resource {
       protected Date updated;
       protected String description;
    
-      /** 
-       * @see Extension#getNamespace()
-       */
       public T namespace(URI namespace) {
          this.namespace = namespace;
          return self();
       }
 
-      /** 
-       * @see Extension#getAlias()
-       */
       public T alias(String alias) {
          this.alias = alias;
          return self();
       }
 
-      /**
-       * @see Extension#getAlias()
-       */
       @Override
       public T id(String id) {
          return alias(id);
       }
 
-      /** 
-       * @see Extension#getUpdated()
-       */
       public T updated(Date updated) {
          this.updated = updated;
          return self();
       }
 
-      /** 
-       * @see Extension#getDescription()
-       */
       public T description(String description) {
          this.description = description;
          return self();
@@ -157,17 +116,19 @@ public class Extension extends Resource {
    @Override
    public boolean equals(Object obj) {
       if (this == obj) return true;
-      if (obj == null || getClass() != obj.getClass()) return false;
-      Extension that = Extension.class.cast(obj);
-      return super.equals(that) && Objects.equal(this.namespace, that.namespace)
-               && Objects.equal(this.alias, that.alias)
-               && Objects.equal(this.updated, that.updated)
-               && Objects.equal(this.description, that.description);
+      if (!(obj instanceof Extension)) return false;
+      Extension that = (Extension) obj;
+      return super.equals(that) && Objects.equal(namespace, that.namespace)
+               && Objects.equal(alias, that.alias)
+               && Objects.equal(updated, that.updated)
+               && Objects.equal(description, that.description);
    }
    
    protected ToStringHelper string() {
       return super.string()
-            .add("namespace", namespace).add("alias", alias).add("updated", updated).add("description", description);
+            .add("namespace", namespace)
+            .add("alias", alias)
+            .add("updated", updated)
+            .add("description", description);
    }
-   
 }
