8555d5cab0fec560157d8c2cab207f0ba54fd5db
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 19:38:22 -0500

    Your changing file apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/functions/internal/ParseServices.java

--- apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/functions/internal/ParseServices.java
+++ apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/functions/internal/ParseServices.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.openstack.keystone.v2_0.functions.internal;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -30,7 +15,6 @@ import org.jclouds.json.Json;
 import org.jclouds.openstack.keystone.v2_0.KeystoneApi;
 import org.jclouds.openstack.keystone.v2_0.domain.Service;
 import org.jclouds.openstack.keystone.v2_0.extensions.ServiceAdminApi;
-import org.jclouds.openstack.keystone.v2_0.functions.internal.ParseServices.Services;
 import org.jclouds.openstack.v2_0.domain.Link;
 import org.jclouds.openstack.v2_0.domain.PaginatedCollection;
 import org.jclouds.openstack.v2_0.options.PaginationOptions;
@@ -40,54 +24,57 @@ import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.inject.TypeLiteral;
 
-/**
- * boiler plate until we determine a better way
- */
 @Beta
 @Singleton
-public class ParseServices extends ParseJson<Services> {
-   static class Services extends PaginatedCollection<Service> {
+public class ParseServices extends ParseJson<ParseServices.Services> {
+    
+    static class Services extends PaginatedCollection<Service> {
 
-      @ConstructorProperties({ "OS-KSADM:services", "services_links" })
-      protected Services(Iterable<Service> services, Iterable<Link> services_links) {
-         super(services, services_links);
-      }
+        @ConstructorProperties({ "OS-KSADM:services", "services_links" })
+        protected Services(Iterable<Service> services, Iterable<Link> services_links) {
+            super(services, services_links);
+        }
+    }
 
-   }
+    @Inject
+    public ParseServices(Json json) {
+        super(json, TypeLiteral.get(Services.class));
+    }
 
-   @Inject
-   public ParseServices(Json json) {
-      super(json, TypeLiteral.get(Services.class));
-   }
+    public static class ToPagedIterable extends Arg0ToPagedIterable.FromCaller<Service, ToPagedIterable> {
 
-   public static class ToPagedIterable extends Arg0ToPagedIterable.FromCaller<Service, ToPagedIterable> {
+        private final KeystoneApi api;
 
-      private final KeystoneApi api;
+        @Inject
+        protected ToPagedIterable(KeystoneApi api) {
+            this.api = checkNotNull(api, "api");
+        }
 
-      @Inject
-      protected ToPagedIterable(KeystoneApi api) {
-         this.api = checkNotNull(api, "api");
-      }
+        @Override
+        protected Function<Object, IterableWithMarker<Service>> markerToNextForArg0(Optional<Object> ignored) {
+            final ServiceAdminApi serviceApi = api.getServiceAdminApi().get();
+            return new ListServicesFunction(serviceApi);
+        }
 
-      @Override
-      protected Function<Object, IterableWithMarker<Service>> markerToNextForArg0(Optional<Object> ignored) {
-         final ServiceAdminApi serviceApi = api.getServiceAdminApi().get();
-         return new Function<Object, IterableWithMarker<Service>>() {
+        private static class ListServicesFunction implements Function<Object, IterableWithMarker<Service>> {
+
+            private final ServiceAdminApi serviceApi;
+
+            ListServicesFunction(ServiceAdminApi serviceApi) {
+                this.serviceApi = serviceApi;
+            }
 
             @SuppressWarnings("unchecked")
             @Override
             public IterableWithMarker<Service> apply(Object input) {
-               PaginationOptions paginationOptions = PaginationOptions.class.cast(input);
-               return IterableWithMarker.class.cast(serviceApi.list(paginationOptions));
+                PaginationOptions paginationOptions = PaginationOptions.class.cast(input);
+                return IterableWithMarker.class.cast(serviceApi.list(paginationOptions));
             }
 
             @Override
             public String toString() {
-               return "listServices()";
+                return "listServices()";
             }
-         };
-      }
-
-   }
-
+        }
+    }
 }
