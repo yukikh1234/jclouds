6f199b8aac6ba8c38b0c44c55126bbef1b6cd140
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 19:31:14 -0500

    Your changing file apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/config/KeystoneParserModule.java

--- apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/config/KeystoneParserModule.java
+++ apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/config/KeystoneParserModule.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.openstack.keystone.v2_0.config;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -43,18 +28,12 @@ public class KeystoneParserModule extends AbstractModule {
       bind(SetTypeAdapterFactory.class).to(ValuesSetTypeAdapterFactory.class);
    }
 
-   /**
-    * Handles the goofy structures with "values" holder wrapping an array
-    * http://docs.openstack.org/api/openstack-identity-service/2.0/content/Versions-d1e472.html
-    * <p/>
-    * Treats [A,B,C] and {"values"=[A,B,C], "someotherstuff"=...} as the same Set
-    */
    public static class ValuesSetTypeAdapterFactory extends SetTypeAdapterFactory {
 
       @Override
       @SuppressWarnings("unchecked")
       protected <E, I> TypeAdapter<I> newAdapter(TypeAdapter<E> elementAdapter) {
-         return (TypeAdapter<I>) new Adapter<E>(elementAdapter);
+         return (TypeAdapter<I>) new Adapter<>(elementAdapter);
       }
 
       public static final class Adapter<E> extends TypeAdapter<Set<E>> {
@@ -62,35 +41,35 @@ public class KeystoneParserModule extends AbstractModule {
          private final SetTypeAdapter<E> delegate;
 
          public Adapter(TypeAdapter<E> elementAdapter) {
-            this.delegate = new SetTypeAdapter<E>(elementAdapter);
+            this.delegate = new SetTypeAdapter<>(elementAdapter);
             nullSafe();
          }
 
          public void write(JsonWriter out, Set<E> value) throws IOException {
-            this.delegate.write(out, value);
+            delegate.write(out, value);
          }
 
          @Override
          public Set<E> read(JsonReader in) throws IOException {
-            if (in.peek() == JsonToken.BEGIN_OBJECT) {
-               Builder<E> builder = ImmutableSet.<E>builder();
-               boolean foundValues = false;
-               in.beginObject();
-               while (in.hasNext()) {
-                  String name = in.nextName();
-                  if (Objects.equal("values", name)) {
-                     foundValues = true;
-                     builder.addAll(delegate.read(in));
-                  } else {
-                     in.skipValue();
-                  }
+            return in.peek() == JsonToken.BEGIN_OBJECT ? readObject(in) : delegate.read(in);
+         }
+
+         private Set<E> readObject(JsonReader in) throws IOException {
+            Builder<E> builder = ImmutableSet.builder();
+            boolean foundValues = false;
+            in.beginObject();
+            while (in.hasNext()) {
+               String name = in.nextName();
+               if (Objects.equal("values", name)) {
+                  foundValues = true;
+                  builder.addAll(delegate.read(in));
+               } else {
+                  in.skipValue();
                }
-               checkState(foundValues, "Expected BEGIN_ARRAY or the object to contain an array called 'values'");
-               in.endObject();
-               return builder.build();
-            } else {
-               return delegate.read(in);
             }
+            checkState(foundValues, "Expected BEGIN_ARRAY or the object to contain an array called 'values'");
+            in.endObject();
+            return builder.build();
          }
       }
    }
