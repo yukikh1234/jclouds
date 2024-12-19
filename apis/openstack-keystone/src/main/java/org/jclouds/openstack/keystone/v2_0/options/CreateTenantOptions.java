ad97597cc0a7732b6c1ef436e7e3eef7899b9851
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 19:26:22 -0500

    Your changing file apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/options/CreateTenantOptions.java

--- apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/options/CreateTenantOptions.java
+++ apis/openstack-keystone/src/main/java/org/jclouds/openstack/keystone/v2_0/options/CreateTenantOptions.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.openstack.keystone.v2_0.options;
 
 import static com.google.common.base.Objects.equal;
@@ -47,9 +32,8 @@ public class CreateTenantOptions implements MapBinder {
       if (object instanceof CreateTenantOptions) {
          final CreateTenantOptions other = CreateTenantOptions.class.cast(object);
          return equal(description, other.description) && equal(enabled, other.enabled);
-      } else {
-         return false;
       }
+      return false;
    }
 
    @Override
@@ -58,10 +42,10 @@ public class CreateTenantOptions implements MapBinder {
    }
 
    protected ToStringHelper string() {
-      ToStringHelper toString = MoreObjects.toStringHelper("").omitNullValues();
-      toString.add("description", description);
-      toString.add("enabled", Boolean.valueOf(enabled));
-      return toString;
+      return MoreObjects.toStringHelper(this)
+            .omitNullValues()
+            .add("description", description)
+            .add("enabled", enabled);
    }
 
    @Override
@@ -77,30 +61,27 @@ public class CreateTenantOptions implements MapBinder {
       private ServerRequest(String name) {
          this.name = name;
       }
-
    }
 
    @Override
    public <R extends HttpRequest> R bindToRequest(R request, Map<String, Object> postParams) {
-      ServerRequest tenant = new ServerRequest(checkNotNull(postParams.get("name"), "name parameter not present")
-            .toString());
-      if (description != null)
-         tenant.description = description;
-      tenant.enabled = enabled;
-
+      ServerRequest tenant = createServerRequestFromParams(postParams);
       return bindToRequest(request, (Object) ImmutableMap.of("tenant", tenant));
    }
 
-   /**
-    * Gets the default tenant description
-    */
+   private ServerRequest createServerRequestFromParams(Map<String, Object> postParams) {
+      ServerRequest tenant = new ServerRequest(checkNotNull(postParams.get("name"), "name parameter not present").toString());
+      if (description != null) {
+         tenant.description = description;
+      }
+      tenant.enabled = enabled;
+      return tenant;
+   }
+
    public String getDescription() {
       return this.description;
    }
 
-   /**
-    * A description can be defined when creating a tenant.
-    */
    public CreateTenantOptions description(String description) {
       this.description = description;
       return this;
@@ -116,23 +97,13 @@ public class CreateTenantOptions implements MapBinder {
    }
 
    public static class Builder {
-
-      /**
-       * @see CreateTenantOptions#description(String)
-       */
       public static CreateTenantOptions description(String description) {
-         CreateTenantOptions options = new CreateTenantOptions();
-         return options.description(description);
+         return new CreateTenantOptions().description(description);
       }
 
-      /**
-       * @see CreateTenantOptions#enabled(boolean)
-       */
       public static CreateTenantOptions enabled(boolean enabled) {
-         CreateTenantOptions options = new CreateTenantOptions();
-         return options.enabled(enabled);
+         return new CreateTenantOptions().enabled(enabled);
       }
-
    }
 
    @Override
