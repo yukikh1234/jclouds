85edd823fb19ecc75cf311204e6b30de09596359
yukikh1234 <khouloud.oueslati@polymtl.ca>
2024-12-18 19:17:09 -0500

    Your changing file apis/openstack-keystone/src/main/java/org/jclouds/openstack/v2_0/domain/Limits.java

--- apis/openstack-keystone/src/main/java/org/jclouds/openstack/v2_0/domain/Limits.java
+++ apis/openstack-keystone/src/main/java/org/jclouds/openstack/v2_0/domain/Limits.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jclouds.openstack.v2_0.domain;
 
 import static com.google.common.base.Objects.equal;
@@ -55,16 +40,21 @@ public final class Limits {
 
    @Override
    public boolean equals(Object obj) {
-      if (this == obj)
+      if (this == obj) {
          return true;
-      if (obj == null || getClass() != obj.getClass())
+      }
+      if (!(obj instanceof Limits)) {
          return false;
-      Limits that = Limits.class.cast(obj);
-      return equal(this.rateLimits, that.rateLimits) && equal(this.absoluteLimits, that.absoluteLimits);
+      }
+      Limits that = (Limits) obj;
+      return equal(rateLimits, that.rateLimits) && equal(absoluteLimits, that.absoluteLimits);
    }
 
    @Override
    public String toString() {
-      return toStringHelper(this).add("rateLimits", rateLimits).add("absoluteLimits", absoluteLimits).toString();
+      return toStringHelper(this)
+            .add("rateLimits", rateLimits)
+            .add("absoluteLimits", absoluteLimits)
+            .toString();
    }
 }
